c33c1f25ac1f450d55b3881f09f85ffe10987469
Daniel Diaz <diaz.carrete@facebook.com>
2017-03-01 23:18:19 +0100

    First compilation.

--- NOTES.md
+++ NOTES.md
@@ -4,4 +4,5 @@ Links
 - [GHC user guide](https://downloads.haskell.org/~ghc/8.0.2/docs/html/)
 - [Extending GHC](https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/extending_ghc.html)
 - [GHC haddocks](https://downloads.haskell.org/~ghc/8.0.2/docs/html/libraries/ghc-8.0.2/index.html)
+- [GHC as a library](https://wiki.haskell.org/GHC/As_a_library)
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,22 @@
-module Main where
-
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import GHC
+import GHC.Paths (libdir)
+import DynFlags
+ 
+main = 
+    defaultErrorHandler defaultFatalMessager defaultFlushOut $
+      runGhc (Just libdir) $ 
+        do dflags <- getSessionDynFlags
+           setSessionDynFlags dflags
+           target <- guessTarget "./data/Main.hs" Nothing
+           setTargets [target]
+           load LoadAllTargets
+           modSum <- getModSummary $ mkModuleName "Main"
+           p <- parseModule modSum
+           t <- typecheckModule p
+           d <- desugarModule t
+           l <- loadModule d
+           n <- getNamesInScope
+           c <- return $ coreModule d
+           g <- getModuleGraph
+           mapM showModule g     
+           return ()
